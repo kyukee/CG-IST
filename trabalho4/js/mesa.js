d7db543337f4b5cf0568bcbb539575f2dba89338
kyukee <iulian.p234@hotmail.com>
2018-11-15 00:33:57 +0000

    do most of points 1 and 2 from project 4

--- trabalho4/js/mesa.js
+++ trabalho4/js/mesa.js
@@ -1,17 +1,20 @@
 /*global THREE, requestAnimationFrame, console*/
 
 var camera, scene, renderer;
-
 var cameraTop;
 
-var geometry, material, mesh;
-
+var geometry, material, mesh, texture, bumpTexture;
+var meshes = [], basicMats = [], phongMats = [];
 var clock;
-
 var controls;
 
-var tableMaterialPhong, tableMaterialBasic;
+var table, tableMaterialPhong, tableMaterialBasic;
+var ball, ballMaterialPhong, ballMaterialBasic;
+var cube, cubeMaterialPhong, cubeMaterialBasic;
+var currentMaterial = " ";
+var orbit = true;
 
+var directionalLight, spotlight;
 
 
 ////////////////////////////////////////////////
@@ -21,32 +24,140 @@ var tableMaterialPhong, tableMaterialBasic;
 function createTable(x, y, z) {
     'use strict';
     
-    var table = new THREE.Object3D();
-    
+    table = new THREE.Object3D();
+
+    texture = new THREE.TextureLoader().load("img/checkers.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(5, 5);
+
     tableMaterialPhong = new THREE.MeshPhongMaterial({
-        color: 0xff0000,
+        color: 0xffffff,
         emissive: 0x000000,
-        shininess: 50
+        shininess: 70,
+        map: texture
     })
+    phongMats.push(tableMaterialPhong);
 
     tableMaterialBasic = new THREE.MeshBasicMaterial({
-        color: 0xff0000, 
+        color: 0xffffff, 
         opacity: 1, 
-        wireframe: false
+        wireframe: false,
+        map: texture
     })
+    basicMats.push(tableMaterialBasic);
 
     // width, height, depth
-    geometry = new THREE.CubeGeometry(60, 2, 60); 
-    mesh = new THREE.Mesh(geometry, tableMaterialBasic);
+    geometry = new THREE.CubeGeometry(100, 1, 100); 
+    mesh = new THREE.Mesh(geometry, tableMaterialPhong);
     mesh.position.set(x, y, z);
+    table.receiveShadow = true;
+    meshes.push(mesh);
     table.add(mesh);
-
-
     scene.add(table);
     
     table.position.x = x;
     table.position.y = y;
     table.position.z = z;
+}
+
+
+////////////////////////////////////////////////
+//////              BALL                  //////
+////////////////////////////////////////////////
+
+function createBall(x, y, z) {
+    'use strict';
+    
+    ball = new THREE.Object3D();
+
+    texture = new THREE.TextureLoader().load("img/ball.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1, 1);
+
+    ballMaterialPhong = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        emissive: 0x000000,
+        shininess: 180,
+        map: texture
+    })
+    phongMats.push(ballMaterialPhong);
+
+    ballMaterialBasic = new THREE.MeshBasicMaterial({
+        color: 0xffffff, 
+        opacity: 1, 
+        wireframe: false,
+        map: texture
+    })
+    basicMats.push(ballMaterialBasic);
+
+    // radius, width segments, height segments
+    geometry = new THREE.SphereGeometry(8, 12, 12); 
+    mesh = new THREE.Mesh(geometry, ballMaterialPhong);
+    mesh.position.set(x, y, z);
+    mesh.castShadow = true;
+    meshes.push(mesh);
+    ball.add(mesh);
+    ball.castShadow = true;
+    scene.add(ball);
+    
+    ball.position.x = x;
+    ball.position.y = y;
+    ball.position.z = z;
+}
+
+
+////////////////////////////////////////////////
+//////              CUBE                  //////
+////////////////////////////////////////////////
+
+function createCube(x, y, z) {
+    'use strict';
+    
+    cube = new THREE.Object3D();
+
+    texture = new THREE.TextureLoader().load("img/crate-texture.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1, 1);
+
+    bumpTexture = new THREE.TextureLoader().load("img/checkers.jpg");
+    bumpTexture.wrapS = THREE.RepeatWrapping;
+    bumpTexture.wrapT = THREE.RepeatWrapping;
+    bumpTexture.repeat.set(1, 1);
+
+    cubeMaterialPhong = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        emissive: 0x000000,
+        shininess: 90,
+        wireframe: false,
+        map: texture,
+        bumpMap: bumpTexture
+    })
+    phongMats.push(cubeMaterialPhong);
+
+    cubeMaterialBasic = new THREE.MeshBasicMaterial({
+        color: 0xffffff, 
+        opacity: 1, 
+        wireframe: false,
+        map: texture
+    })
+    basicMats.push(cubeMaterialBasic);
+
+    // width, height, depth
+    geometry = new THREE.CubeGeometry(15, 15, 15);
+    mesh = new THREE.Mesh(geometry, cubeMaterialPhong);
+    mesh.position.set(x, y, z);
+    mesh.castShadow = true;
+    meshes.push(mesh);
+    cube.add(mesh);
+    cube.castShadow = true;
+    scene.add(cube);
+    
+    cube.position.x = x;
+    cube.position.y = y;
+    cube.position.z = z;
 }
 
 
@@ -65,8 +176,9 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
     
-    createTable(0, 10, 0);
-
+    createTable(0, 0, 0);
+    createBall(10, 10, 5);
+    createCube(-5, 8, -10);
 }
 
 
@@ -80,14 +192,59 @@ function createCamera() {
     var camFactor = 14;
 
     cameraTop = new THREE.OrthographicCamera( window.innerWidth / - camFactor, window.innerWidth / camFactor, window.innerHeight / camFactor, window.innerHeight / - camFactor, 1, 1000 );
-    cameraTop.position.set(0,50,0);
-    cameraTop.lookAt(scene.position);
+    cameraTop.position.set(-40,30,60);
+    cameraTop.lookAt(new THREE.Vector3(0,0,0));
 
     camera = cameraTop;
 
     controls = new THREE.OrbitControls( cameraTop );
     controls.update();
 
+}
+
+
+////////////////////////////////////////////////
+//////              LIGHTS                //////
+////////////////////////////////////////////////
+
+function createLights() {
+    'use strict';
+    
+    // color, intensity
+    directionalLight = new THREE.DirectionalLight(0xffffff, 1.1);
+    directionalLight.position.set(40, 100, 40);
+    directionalLight.target = table;
+    directionalLight.castShadow = true;
+    directionalLight.shadowDarkness = 0.5;
+    directionalLight.visible = false;
+    scene.add(directionalLight);
+
+    directionalLight.shadow.mapSize.width = 512;
+    directionalLight.shadow.mapSize.height = 512;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 500;
+    directionalLight.shadowCameraLeft = -60;
+    directionalLight.shadowCameraRight = 60;
+    directionalLight.shadowCameraTop = 60;
+    directionalLight.shadowCameraBottom = -60;
+
+    // color, intensity, distance, angle, penumbra
+    spotlight = new THREE.SpotLight(0xffffff, 1, 2000, 0.25, 0.2);
+    spotlight.position.set(-80,140,-30);
+    spotlight.target = table;
+    spotlight.castShadow = true;
+    scene.add(spotlight);
+
+
+    renderer.shadowMap.enabled = true;                  
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowCameraNear = 3;
+    renderer.shadowCameraFar = 5;
+    renderer.shadowCameraFov = 50;
+    renderer.shadowMapBias = 0.0039;
+    renderer.shadowMapDarkness = 0.5;
+    renderer.shadowMapWidth = 1024;
+    renderer.shadowMapHeight = 1024;
 }
 
 function onResize() {
@@ -110,44 +267,65 @@ function onKeyDown(e) {
     'use strict';
     
     switch (e.keyCode) {
-    case 65: //A
-    case 97: //a
+    case 68: //D
+    case 100: //d
+        if(directionalLight.visible) {
+            directionalLight.visible = false;
+        } else {
+            directionalLight.visible = true;
+        }
 
-        scene.traverse(function (node) {
-            if (node instanceof THREE.Mesh) {
-                node.material.wireframe = !node.material.wireframe;
+        break;
+    case 80: //P
+    case 112: //p
+        if(spotlight.visible) {
+            spotlight.visible = false;
+        } else {
+            spotlight.visible = true;
+        }
+        break;
+
+    case 76: //L
+    case 108: //l
+
+        if (currentMaterial == "basic") {
+
+            // passar tudo para phong
+            for (var i = 0, len = meshes.length; i < len; i++) {
+                var mesh = meshes[i];
+                var phong = phongMats[i];
+                mesh.material = phong;
+                currentMaterial = "phong";
             }
-        });
+        }else{
+            // passar tudo para basic
+            for (var i = 0, len = meshes.length; i < len; i++) {
+
+                var mesh = meshes[i];
+                var basic = basicMats[i];
+                mesh.material = basic;
+                currentMaterial = "basic";
+            }
+        }
 
         break;
-    case 69:  //E
-    case 101: //e
-        scene.traverse(function (node) {
-            if (node instanceof THREE.AxisHelper) {
-                node.visible = !node.visible;
-            }
-        });
+
+    case 87: //W
+    case 119: //w
+
+        for (var i = 0, len = meshes.length; i < len; i++) {
+
+            var basic = basicMats[i];
+            var phong = phongMats[i];
+            basic.wireframe = !basic.wireframe;
+            phong.wireframe = !phong.wireframe;
+        }
+
         break;
-    case 49: //1
-        camera = cameraFront;
-        break;
-    case 50: //2
-        camera = cameraSide;
-        break;
-    case 51: //3
-        camera = cameraTop;
-        break;
-    case 37: //left arrow
-        chair.userData.rotateLeft = true;
-        break;
-    case 38: //up arrow
-        chair.userData.accFront = chair.userData.accelaration;
-        break;
-    case 39: //right arrow
-        chair.userData.rotateRight = true;
-        break;
-    case 40: //down arrow
-        chair.userData.accBack = chair.userData.accelaration;
+
+    case 79: //O
+    case 111: //o
+        orbit = !orbit;
         break;
     }
 }
@@ -186,7 +364,7 @@ function init() {
    
     createScene();
     createCamera();
-
+    createLights();
 
     clock = new THREE.Clock(true);
 
@@ -202,8 +380,15 @@ function animate() {
 
     controls.update();
 
+    if (orbit) {
+        var x = camera.position.x, z = camera.position.z;
+        var rotSpeed = 0.002;
+
+        camera.position.x = x * Math.cos(rotSpeed) + z * Math.sin(rotSpeed);
+        camera.position.z = z * Math.cos(rotSpeed) - x * Math.sin(rotSpeed);
+    }
+
     render();
-    
     requestAnimationFrame(animate);
 }
 
