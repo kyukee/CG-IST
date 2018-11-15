a51afd63c80ec4aa6edb6fff064c32cf2a50bdf5
kyukee <iulian.p234@hotmail.com>
2018-11-15 21:37:11 +0000

    finish points 1 and 2 from project 4

--- trabalho4/js/mesa.js
+++ trabalho4/js/mesa.js
@@ -71,7 +71,7 @@ function createBall(x, y, z) {
     
     ball = new THREE.Object3D();
 
-    texture = new THREE.TextureLoader().load("img/ball.jpg");
+    texture = new THREE.TextureLoader().load("img/ball.png");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(1, 1);
@@ -89,11 +89,10 @@ function createBall(x, y, z) {
         for ( var j = 0; j < 3; j ++ ) {
 
             // transforma coordenadas de [-1, 1] para [0, 1]
-            uvs[j].x = face.vertexNormals[j].x * 0.5 + 0.5 + ((uvs[j].z < 0) ? 1 : 0);
+            uvs[j].x = (face.vertexNormals[j].x * 0.5 + 0.5) * 0.5 + ((face.vertexNormals[j].z < 0) ? 0.5 : 0);
             uvs[j].y = face.vertexNormals[j].y * 0.5 + 0.5;
         }
     }
-
 
     ballMaterialPhong = new THREE.MeshPhongMaterial({
         color: 0xffffff,
@@ -134,23 +133,241 @@ function createCube(x, y, z) {
     
     cube = new THREE.Object3D();
 
-    texture = new THREE.TextureLoader().load("img/crate-texture.jpg");
+    texture = new THREE.TextureLoader().load("img/cube.png");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(1, 1);
 
-    bumpTexture = new THREE.TextureLoader().load("img/checkers.jpg");
+    // width, height, depth
+    geometry = new THREE.CubeGeometry(15, 15, 15);
+
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+
+    // geometry.faceVertexUvs[0][faceIndex][vertexIndex]
+    var faceVertexUvs = geometry.faceVertexUvs[ 0 ];        // material 0
+
+    var blocksH = 4;    // numero de colunas da textura
+    var blocksV = 3;    // numero de linhas da textura
+    var offsetX;        // numero da face do quadrado na textura, a contar da esquerda 
+    var offsetY;        // numero da face do quadrado na textura, a contar de baixo
+
+    var v_x=[],v_y=[],v_z=[];
+
+    for ( var i = 0; i < faceVertexUvs.length; i ++ ) {     // itera sobre faces
+        // transforma coordenadas de [-1, 1] para [0, 1]
+
+        var uvs = faceVertexUvs[i];
+        var face = geometry.faces[i];
+
+        for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+            v_x.push((face.vertexNormals[j].x > 0) ? 1 : -1);
+            v_y.push((face.vertexNormals[j].y > 0) ? 1 : -1);
+            v_z.push((face.vertexNormals[j].z > 0) ? 1 : -1);
+        }
+
+        // faces perpendiculares a x
+        if (v_x[0] > 0 && v_x[1] > 0 && v_x[2] > 0) {
+
+            // canto inferior esquerdo da face do quadrado correpondente, na textura
+            offsetX = 0;
+            offsetY = 1;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_y[j] == 1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_y[j] == 1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_y[j] == -1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_y[j] == -1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                }
+            }
+
+        } else if (v_x[0] < 0 && v_x[1] < 0 && v_x[2] < 0) {
+
+            offsetX = 2;
+            offsetY = 1;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_y[j] == 1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_y[j] == 1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_y[j] == -1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_y[j] == -1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                }
+            }
+
+        // faces perpendiculares a y
+        } else if (v_y[0] > 0 && v_y[1] > 0 && v_y[2] > 0) {
+            
+            offsetX = 1;
+            offsetY = 2;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_x[j] == 1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == 1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_x[j] == -1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == -1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                }
+            }
+
+        } else if (v_y[0] < 0 && v_y[1] < 0 && v_y[2] < 0) {
+            
+            offsetX = 1;
+            offsetY = 0;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_x[j] == 1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_x[j] == 1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == -1 && v_z[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_x[j] == -1 && v_z[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                }
+            }
+
+        // faces perpendiculares a z
+        } else if (v_z[0] > 0 && v_z[1] > 0 && v_z[2] > 0) {
+            
+            offsetX = 3;
+            offsetY = 1;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_x[j] == 1 && v_y[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == 1 && v_y[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_x[j] == -1 && v_y[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == -1 && v_y[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                }
+            }
+
+        } else if (v_z[0] < 0 && v_z[1] < 0 && v_z[2] < 0) {
+            
+            offsetX = 1;
+            offsetY = 1;
+
+            for ( var j = 0; j < uvs.length; j ++ ) {           // itera sobre vertices
+
+                if (v_x[j] == 1 && v_y[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == 1 && v_y[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * offsetX;
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                } else if (v_x[j] == -1 && v_y[j] == 1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * (offsetY+1);
+
+                } else if (v_x[j] == -1 && v_y[j] == -1) {
+
+                    uvs[j].x = 1/ blocksH * (offsetX+1);
+                    uvs[j].y = 1/ blocksV * offsetY;
+
+                }
+            }
+        }
+
+        v_x=[],v_y=[],v_z=[];
+    }
+
+
+
+    bumpTexture = new THREE.TextureLoader().load("img/cube-bump.png");
     bumpTexture.wrapS = THREE.RepeatWrapping;
     bumpTexture.wrapT = THREE.RepeatWrapping;
-    bumpTexture.repeat.set(1, 1);
+    bumpTexture.repeat.set(3, 4);
 
     cubeMaterialPhong = new THREE.MeshPhongMaterial({
         color: 0xffffff,
         emissive: 0x000000,
         shininess: 90,
         wireframe: false,
-        map: texture,
-        bumpMap: bumpTexture
+        bumpMap: bumpTexture,
+        map: texture
     })
     phongMats.push(cubeMaterialPhong);
 
@@ -162,8 +379,6 @@ function createCube(x, y, z) {
     })
     basicMats.push(cubeMaterialBasic);
 
-    // width, height, depth
-    geometry = new THREE.CubeGeometry(15, 15, 15);
     mesh = new THREE.Mesh(geometry, cubeMaterialPhong);
     mesh.position.set(x, y, z);
     mesh.castShadow = true;
@@ -193,9 +408,9 @@ function createScene() {
 
     scene.add(new THREE.AxisHelper(10));
     
-    createTable(0, 0, 0);
-    createBall(10, 10, 5);
-    createCube(-5, 8, -10);
+    createTable(0, -2, 0);
+    createBall(10, 8, 5);
+    createCube(-5, 6, -10);
 }
 
 
@@ -249,12 +464,11 @@ function createLights() {
     directionalLight.shadowCameraBottom = -60;
 
     // color, intensity, distance, angle, penumbra
-    spotlight = new THREE.SpotLight(0xffffff, 1, 2000, 0.25, 0.2);
+    spotlight = new THREE.SpotLight(0xffffff, 1.5, 2000, 0.25, 1);
     spotlight.position.set(-80,140,-30);
     spotlight.target = table;
     spotlight.castShadow = true;
     scene.add(spotlight);
-
 
     renderer.shadowMap.enabled = true;                  
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
